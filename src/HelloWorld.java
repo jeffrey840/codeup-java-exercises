d68380b222711c03a131eb50f980f97b14880b4b
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-07 16:13:02 -0600

    completed exercise 2

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World! & ");
+        System.out.print("Hey there, again. ");
+
+        int myFavoriteNumber = 25;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Here is my string";
+        System.out.println(myString);
+
+
+
+
+    }
+
+
+}
+
+
