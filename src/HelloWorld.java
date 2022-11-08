3f4bb89c9563623f3df22958b241d6c3ba9a1489
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-07 19:17:36 -0600

    completed exercise 12

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,10 +11,35 @@ public class HelloWorld {
         System.out.println(myFavoriteNumber);
 
         String myString = "Here is my string";
+//        String myString = w;
+//        String myString = 3.14159;
         System.out.println(myString);
 
-        long myNumber = 123;
+        double myNumber = 3.14;
         System.out.println(myNumber);
+
+
+//        first output was 5 and 6
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        second output was 6 and 6
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        returns an error : Exception in thread "main" java.lang.ClassCastException: class java.lang.String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are in module java.base of loader 'bootstrap')
+//	at HelloWorld.main(HelloWorld.java:36)
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+
+//        returns error : java: incompatible types: java.lang.String cannot be converted to int.
+//        int three = (int) "three";
 
 
 
