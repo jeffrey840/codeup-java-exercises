dcbf1fdb18cb48f5e1438bb5afa992f737629d3e
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-09 14:21:01 -0600

    completed bob exercises

---
+++ src/Bob.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Say something to Bob? ");
+        String answer = scanner.nextLine().trim();
+        System.out.println(answer);
+        if(answer.endsWith("?")) {
+            System.out.println("Sure.");
+        } else if (answer.endsWith("!")){
+            System.out.println("Whoa, chill out!");
+//            when user presses enter
+        } else if (answer.equals("")){
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever.");
+        }
+
+    }
+
+}
