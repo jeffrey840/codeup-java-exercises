c64046325b89d419faf1bdcd4a60fe834517778d
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-09 11:44:25 -0600

    completed 3rd exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -17,11 +17,62 @@ public class ControlFlowExercises {
 //
 //        5 6 7 8 9 10 11 12 13 14 15
 
-        int i = 5;
-        while (i <= 15) {
-            System.out.print( " " + i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print( " " + i);
+//            i++;
+//        }
+
+        //        Do While
+//
+//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//        DO WHILE LOOP:
+//       int  num = 0;
+//        do {
+//            System.out.println(num);
+//            num += 2;
+//        } while (num <= 100);
+
+//        FOR LOOP:
+//        for(int i = 0; i <=100; i += 2){
+//            System.out.println(i);
+//        }
+
+//        Alter your loop to count backwards by 5's from 100 to -10.
+//        DO WHILE LOOP
+//       int  num = 100;
+//        do {
+//            System.out.println(num);
+//            num -=5;
+//        } while (num >= -10);
+
+//        FOR LOOP
+//        for(int i = 100; i >= -10; i -=5){
+//            System.out.println(i);
+//        }
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+//        DO WHILE LOOP:
+//        long num = 2;
+//        do {
+//            System.out.println(num);
+//            num *= num;
+//        } while (num <= 1000000);
+//
+//        FOR LOOP:
+//        for(long i = 2; i < 1000000; i *= i){
+//            System.out.println(i);
+//        }
+//
+//        2
+//        4
+//        16
+//        256
+//        65536
+
+
+//        For
+//
+//        Refactor the previous two exercises to use a for loop instead.
 
 
     }
@@ -31,34 +82,9 @@ public class ControlFlowExercises {
 
 //=================
 
-//Exercises
 
 //
-//        Loop Basics
-//
-//        While
-//
-//        Create an integer variable i with a value of 5.
-//        Create a while loop that runs so long as i is less than or equal to 15
-//        Each loop iteration, output the current value of i, then increment i by one.
-//        Your output should look like this:
-//
-//        5 6 7 8 9 10 11 12 13 14 15
-//
-//        Do While
-//
-//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
-//        Alter your loop to count backwards by 5's from 100 to -10.
-//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
-//
-//        2
-//        4
-//        16
-//        256
-//        65536
-//        For
-//
-//        Refactor the previous two exercises to use a for loop instead.
+
 //        Fizzbuzz
 //
 //        One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
