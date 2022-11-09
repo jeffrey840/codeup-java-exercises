fb3904574f9302310cdfa7369831a14e2ec85e29
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-09 14:10:01 -0600

    completed string basics

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -6,9 +6,22 @@ public class StringExercise {
         String pink = "We don't need no education\n" +
                 "We don't need no thought control\n";
         System.out.println(pink);
+//        We don't need no education
+//        We don't need no thought control
+
 
         String check = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(check);
+//        Check "this" out!, "s inside of "s!
+
+        String window = "In windows, the main drive is usually C:\\";
+        System.out.println(window);
+//        In windows, the main drive is usually C:\
+
+        String Backslash = "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslash\\\\\\!";;
+        System.out.println(Backslash);
+//      I can do backslashes \, double backslashes \\,
+//      and the amazing triple backslash\\\!
 
     }
 
