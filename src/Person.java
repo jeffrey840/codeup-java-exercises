05bbddd6d8a9358ef5dbe98775a1f08f18ab89d6
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-13 22:37:21 -0600

    added new assigment

---
+++ src/Person.java
@@ -0,0 +1,32 @@
+
+
+public class Person {
+    private String name;
+
+    public String getName(){
+//TODO: return the person's name
+        return this.name;
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println("Hello, " + this.name);
+    }
+//    The class should have a constructor that accepts a `String` value and sets
+//    the person's name to the passed string.
+//
+//    Create a `main` method on the class that creates a new `Person` object and
+//    tests the above methods.
+public Person(String name) {
+    this.name = name;
+}
+
+    public static void main(String[] args) {
+
+
+    }
+}
