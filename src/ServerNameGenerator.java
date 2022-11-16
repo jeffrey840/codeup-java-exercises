4483749b8f9cbba2beff7d63f19e8537271c9da4
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-16 10:41:39 -0600

    added comments on server name

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -6,6 +6,7 @@ public class ServerNameGenerator {
 //    main method
     public static void main(String[] args) {
         System.out.println("Here is your server name:");
+//        prints out server noun and adjective
         System.out.printf("%s-%s", randomElement(nouns), randomElement(adjectives));
 
     }
@@ -38,7 +39,8 @@ public class ServerNameGenerator {
             "Queen"
     };
 
-    //    random element method
+//    random element method
+//    using random() constructor
     public static String randomElement(String[] array){
         int randomIndex = new Random().nextInt(array.length);
         return array[randomIndex];
