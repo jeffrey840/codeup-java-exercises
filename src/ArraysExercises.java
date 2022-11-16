518bb0784a9cd1dc3779cdb263d0c557ab158b0e
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-16 11:39:14 -0600

    added comments on array exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,27 +1,31 @@
 
-
 import java.util.Arrays;
 
 public class ArraysExercises {
+
     public static Person[] addPerson(Person[] people, Person person){
-
-        Person[] oneMorePerson = Arrays.copyOf(people, people.length +1);
-        oneMorePerson[oneMorePerson.length - 1] = person;
-        return oneMorePerson;
-
+//        makes a copy of people array increases by 1
+        Person[] addingPerson = Arrays.copyOf(people, people.length +1);
+//        gets the index of the last person
+        addingPerson[addingPerson.length - 1] = person;
+//        returns an array with a last index with a string of "sam"
+        return addingPerson;
     }
-    public static void main(String[] args) {
-//        int[] numbers = {1, 2, 3, 4, 5};
-//        System.out.println(numbers);
 
+//    main method
+    public static void main(String[] args) {
+
+//        array that holds 3 people
         Person[] people = new Person[3];
         people[0] = new Person("john");
         people[1] = new Person("ronn");
         people[2] = new Person("erin");
 
+//        static method
+//        enhanced for loop to iterate over person
         people = addPerson(people, new Person("sam"));
-        for(Person instructor: people){
-            System.out.println(instructor.getName());
+        for(Person newPer: people){
+            System.out.println(newPer.getName());
         }
     }
 
