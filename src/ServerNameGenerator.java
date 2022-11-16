ba1adc134901efff7f882a7889cc8963c1686520
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-15 22:16:51 -0600

    adding progress

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,6 +1,29 @@
 
 import java.util.Random;
 
-
 public class ServerNameGenerator {
+
+//    main method
+    public static void main(String[] args) {
+        System.out.println("Here is your server name:");
+        System.out.printf("%s-%s", randomElement(nouns), randomElement(adjectives));
+
+    }
+
+//    adjectives array
+    public static String[] adjectives = {
+            "clever",
+            "small",
+            "adorable",
+            "adventurous",
+            "aggressive",
+            "agreeable",
+            "alert",
+            "alive",
+            "annoying",
+            "attractive"
+    };
+
+
+
 }
