8d5d6aad37b6a012cfe3d9172879ea65f78b6600
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-17 23:03:07 -0600

    commiting progress

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,13 @@
 package shapes;
 
 public class Rectangle {
+
+    protected int rectangleLength;
+    protected int rectangleWidth;
+
+    public Rectangle(int length, int width) {
+        this.rectangleLength = length;
+        this.rectangleWidth = width;
+    }
+
 }
