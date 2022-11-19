686ddb08b1c56bcace31adc539847c1095115b2b
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-18 23:37:09 -0600

    commiting progress

--- src/DateFormat/DateFormatConverter.java
+++ src/DateFormat/DateFormatConverter.java
@@ -1,6 +1,35 @@
 package DateFormat;
 
+import java.util.Scanner;
+
+/*
+* **Take in the following format:**
+
+```MM/DD/YYYY```
+
+**Output the following:**
+
+```MonthName DD, YYYY```
+
+**Example:**
+
+input - ```12/01/1999```
+
+output - ```December 12, 1999```
+* */
+
 public class DateFormatConverter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        System.out.println("Enter an integer: ");
+        String nextLn = scanner.nextLine();
+        System.out.println("MM/DD/YYYY");
+    }
+
+
+
 
 
 }
