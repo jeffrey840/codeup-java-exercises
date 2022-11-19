186e408b76b05574ee30d5057b47da7fb9618078
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-18 23:25:45 -0600

    commiting progress

--- src/GroceryList/UPrompt.java
+++ src/GroceryList/UPrompt.java
@@ -1,9 +1,15 @@
 package GroceryList;
 
+import java.util.Scanner;
+
 public class UPrompt {
 
     public static void main(String[] args) {
-        System.out.println("testing");
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        System.out.println("How many items will be entered");
+        int answer = Integer.parseInt(scanner.nextLine().trim());
+//        System.out.println(answer);
     }
 
 }
