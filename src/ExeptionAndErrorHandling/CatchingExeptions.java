3094d50f95484915795c639074b4e4745e8a7912
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-21 22:16:28 -0600

    added code

--- src/ExeptionAndErrorHandling/CatchingExeptions.java
+++ src/ExeptionAndErrorHandling/CatchingExeptions.java
@@ -3,6 +3,7 @@ package ExeptionAndErrorHandling;
 public class CatchingExeptions {
 
 
+//    ===============FIRST EXAMPLE
 //    Here is a generic form of what a try-catch block looks like:
 //    try {
 //        // something that
@@ -12,19 +13,40 @@ public class CatchingExeptions {
 //    }
 
 //    main method
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//
+//        try {
+//            System.out.println("Let's see...");
+//            int result = 1 / 0;
+//            System.out.println("I can divide by zero!");
+//        } catch (ArithmeticException e) {
+//            System.out.println("Math still works!");
+//        }
+////        print out:
+////        Let's see...
+////        Math still works!
+//
+//    }
+//    ==============SECOND EXAMPLE
+public static void main(String[] args) {
 
+    int[] numbers = {1, 2, 3};
+    int x;
 
-        try {
-            System.out.println("Let's see...");
-            int result = 1 / 0;
-            System.out.println("I can divide by zero!");
-        } catch (ArithmeticException e) {
-            System.out.println("Math still works!");
-        }
-//        print out:
-//        Let's see...
-//        Math still works!
-
+    try {
+        x = numbers[100];
+    } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("Caught an array index exception!");
+    } catch (Exception e) {
+        System.out.println("Caught a generic exception!");
+    } finally {
+        System.out.println("This will always run.");
     }
+
+//    PRINTS:
+//    Caught an array index exception!
+//    This will always run.
+}
+
 }
