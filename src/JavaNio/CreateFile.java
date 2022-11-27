84bb5c5d614544f5aa02352540c5bcbfcef2aec1
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-27 17:43:42 -0600

    made a file inside a directory

--- src/JavaNio/CreateFile.java
+++ src/JavaNio/CreateFile.java
@@ -1,5 +1,24 @@
 package JavaNio;
 
+import java.nio.file.*;
+import java.nio.*;
 public class CreateFile {
 
+    public static void main(String[] args) {
+        try {
+            Path p = Paths.get("DataSet/poem.txt");
+            if (Files.exists(p)) {
+                System.out.println("File already exits");
+            } else {
+                Path donePath = Files.createFile(p);
+                System.out.println("file is created" + donePath.toString());
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
+//created file inside DataSet named poem.txt
+
