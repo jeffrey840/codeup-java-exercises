df9abd64a9ed75c183466a1bf736ee6282b7091e
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-28 16:43:26 -0600

    completed setters and getters for groceryItems

--- src/GroceryList/GroceryItem.java
+++ src/GroceryList/GroceryItem.java
@@ -14,8 +14,51 @@ package GroceryList;
 
 public class GroceryItem {
     private String name;
-    private String category;
     private int quantity;
+    private String category;
+
+//    CONSTRUCTOR
+    public GroceryItem() {
+    }
+//    CONSTRUCTOR
+    public GroceryItem(String name, String category, int quantity) {
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+    }
+
+//     METHOD/SETTER
+    public void setName(String name) {
+        this.name = name;
+    }
+//     METHOD/GETTER
+    public String getName() {
+        return name;
+    }
+
+//     METHOD/SETTER
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+//     METHOD/GETTER
+    public String getCategory() {
+        return category;
+    }
+
+//     METHOD/SETTER
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+//         METHOD/GETTER
+    public void getItemInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Category: " + category);
+        System.out.println("Quantity: " + quantity);
+    }
+
 }
 
 
--- src/GroceryList/UPrompt.java
+++ src/GroceryList/UPrompt.java
@@ -5,4 +5,11 @@ import java.util.ArrayList;
 
 public class UPrompt {
 
+    public static  <GroceryItem> void main(String[] args) {
+        System.out.println("Would you like to create a grocery list!");
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+
 }
