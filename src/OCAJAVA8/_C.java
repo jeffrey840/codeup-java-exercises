2fbe9ec81ec5132e1c1d51150665b861e08a52a8
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-05-20 17:13:22 -0500

    added class

---
+++ src/OCAJAVA8/_C.java
@@ -0,0 +1,7 @@
+package OCAJAVA8;
+
+public class _C {
+    private static int $;
+    public static void main(String[] main) {
+//        String a_b; System.out.print($); System.out.print(a_b);
+    } }
